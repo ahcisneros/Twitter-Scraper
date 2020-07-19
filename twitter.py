f920c0d2566b816013471383c221ff7c035d8a53
ahcisneros <57335799+ahcisneros@users.noreply.github.com>
2020-07-19 01:13:40 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,20 +1,26 @@
-from twitterscraper import query_tweets
+import GetOldTweets3 as got
 import datetime as dt
 import pandas as pd
 import os
 from datetime import date
 from calendar import monthrange
 import numpy as np
+from datetime import datetime
+import time
 
 
 def main():
-    """iterates per 2 day of each month of each year and calls the twitter scraping function"""
-    """input the number of years you want to scrape in list_years"""
-    list_years = [2020, 2019, 2018, 2017, 2016]
-    """input key terms in companies list to scrape from """
-    companies = ["bitcoin", "btc", "ethereum", "eth"]  
+    """pip install GetOldTweets3"""
+    list_years = [2020, 2019, 2018, 2017, 2016] """change the number of years it will trace back"""
+    companies = ["amazon", "apple", "netflix", "facebook", "microsoft", "google", "alphabet", "boeing"] """input within the list the terms to be analyzed"""
+    initiate(list_years, companies)
+
+
+def initiate(list_years, companies):
+    """iterates per 1 day of each month of each year and calls the twitter scraping function"""
+    base_df = pd.DataFrame()
     for term in companies:
-        print(f"Analyzing term: {term}")
+        print(f"Analyzing term: {term} \n")
         for year in list_years:
             if year == 2020:
                 current_month = int(str(date.today()).split("-")[1].strip("0"))
@@ -24,69 +30,108 @@ def main():
                 numerical_months = list(range(13))
                 del numerical_months[0]
             for month in numerical_months:
-                print(f"Current year: {year}, Month: {month}")
-                num_days = int(str(monthrange(year, month)).split(", ")[1].replace(")", ""))
+                if year == 2020:
+                    if int(month) == int(str(date.today()).split("-")[1].strip("0")):
+                        num_days = int(str(datetime.date(datetime.now())).split("-")[2])
+                        print(num_days)
+                    else:
+                        num_days = int(str(monthrange(year, month)).split(", ")[1].replace(")", ""))
+                else:
+                    num_days = int(str(monthrange(year, month)).split(", ")[1].replace(")", ""))
+                print(f"Current Year: {year} / Month: {month}")
                 lst = list(range(num_days + 1))
                 del lst[0]
                 lst.append(num_days - 1)
-                lst = np.sort(lst)
+                lst = list(set(np.sort(lst)))
                 for day in lst:
-                    if day == num_days:
-                        if month + 1 == 13:
-                            twitter_scraping_v3(day, 1, month, 1, year, year + 1, term)
+                    if int(day) == num_days:
+                        if int(month) + 1 == 13:
+                            df = twitter_scraping_v3(int(day), int(1), int(month), int(1), int(year),
+                                                     int(year + 1), str(term))
+                            base_df = pd.concat([df, base_df], ignore_index=True)
+                        elif int(year) == 2020 and int(month) == int(str(date.today()).split("-")[1].strip("0")):
+                            df = twitter_scraping(int(day), int(day + 1), int(year), int(month), str(term))
+                            base_df = pd.concat([df, base_df], ignore_index=True)
                         else:
-                            twitter_scraping_v2(day, 1, year, month, month + 1, term)
+                            df = twitter_scraping_v2(int(day), int(1), int(year), int(month),
+                                                     int(month + 1), str(term))
+                            base_df = pd.concat([df, base_df], ignore_index=True)
                     else:
-                        twitter_scraping(day, day + 1, year, month, term)
+                        df = twitter_scraping(int(day), int(day + 1), int(year), int(month), str(term))
+                        base_df = pd.concat([df, base_df], ignore_index=True)
+
+                save_df(base_df, term)
+                print(f"Saving complete for month {month}\n")
 
 
 def twitter_scraping_v3(first_day, second_day, first_month, second_month,
                         first_year, second_year, term):
     """gets called per 2 sets of days for the start and end parameter of the
     twitter scraper function"""
-    lang = "english"
-    limit = 1000
 
     start = dt.date(first_year, first_month, first_day)
     end = dt.date(second_year, second_month, second_day)
-    print(f"start: {start}/ end: {end} / {term}")
 
-    tweets = query_tweets(term, begindate=start, limit=limit, enddate=end, lang=lang)
-    df = pd.DataFrame(t.__dict__ for t in tweets)
-
-    save_df(df, term)
+    return framework_scrapper(start, end, term)
 
 
 def twitter_scraping_v2(first_day, second_day, year, first_month, second_month, term):
     """gets called per 2 sets of days for the start and end parameter of the
     twitter scraper function"""
-    limit = 1000
-    lang = "english"
 
     start = dt.date(year, first_month, first_day)
     end = dt.date(year, second_month, second_day)
-    print(f"start: {start}/ end: {end} / {term}")
 
-    tweets = query_tweets(term, begindate=start, limit=limit, enddate=end, lang=lang)
-    df = pd.DataFrame(t.__dict__ for t in tweets)
-
-    save_df(df, term)
+    return framework_scrapper(start, end, term)
 
 
 def twitter_scraping(first_day, second_day, year, month, term):
     """gets called per 2 sets of days for the start and end parameter of the
     twitter scraper function"""
-    limit = 1000
-    lang = "english"
-
     start = dt.date(year, month, first_day)
     end = dt.date(year, month, second_day)
-    print(f"start: {start}/ end: {end} / {term}")
 
-    tweets = query_tweets(term, begindate=start, limit=limit, enddate=end, lang=lang)
+    return framework_scrapper(start, end, term)
+
+
+def framework_scrapper(start, end, term):
+    print(f"Start: {start} / End: {end} / Term: {term}")
+    count = 1000
+
+    try:
+        df = core_scrapper(start, end, term, count)
+    except:
+        try:
+            print("Waiting 10 min....")
+            time.sleep(600)
+            df = core_scrapper(start, end, term, count)
+
+        except:
+            try:
+                print("Waiting 3 min more....")
+                time.sleep(180)
+                df = core_scrapper(start, end, term, count)
+            except:
+                try:
+                    print("Waiting 10 min more....")
+                    time.sleep(600)
+                    df = core_scrapper(start, end, term, count)
+                except:
+                    pass
+
+    print("Data extraction completed.")
+    print(f"Total Tweets Collected: {len(df)} \n")
+
+    return df
+
+
+def core_scrapper(start, end, term, count):
+    tweet_criteria = got.manager.TweetCriteria().setQuerySearch(term).setSince(str(start)).setUntil(str(end))
+    tweet_criteria = tweet_criteria.setMaxTweets(count)
+    tweets = got.manager.TweetManager.getTweets(tweet_criteria)
     df = pd.DataFrame(t.__dict__ for t in tweets)
 
-    save_df(df, term)
+    return df
 
 
 def save_df(dataframe, term):
@@ -95,10 +140,12 @@ def save_df(dataframe, term):
     if os.path.exists(f"E:\\Tweet Data\\tweets {term}.csv"):
         existing_df = pd.read_csv(f"E:\\Tweet Data\\tweets {term}.csv")
         updated_df = existing_df.append(dataframe, ignore_index=True)
-        no_duplicated_df = updated_df.drop_duplicates('tweet_url', keep='last')
+        no_duplicated_df = updated_df.drop_duplicates('text', keep='last')
         no_duplicated_df.to_csv(f"E:\\Tweet Data\\tweets {term}.csv", mode='w', index=False)
     else:
-        dataframe.to_csv(f"E:\\Tweet Data\\tweets {term}.csv", index=False)
+        dataframe = dataframe.reset_index()
+        no_duplicated_df = dataframe.drop_duplicates('text', keep='last')
+        no_duplicated_df.to_csv(f"E:\\Tweet Data\\tweets {term}.csv", index=False)
 
-        
+
 main()
